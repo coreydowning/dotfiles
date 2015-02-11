430dc23f1ad9e905847321ef98d5b587ccd67221
Corey Downing <downing.c@gmail.com>
2015-02-11 10:50:35 -0700

    hold on tight

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf $(pwd)/zshrc ${HOME}/.zshrc
+ln -sf $(pwd)/vimrc ${HOME}/.vimrc
+ln -sf $(pwd)/vim ${HOME}/.vim
+ln -sf $(pwd)/gitconfig ${HOME}/.gitconfig
+ln -sf $(pwd)/gitignore ${HOME}/.gitignore
---
+++ vim/settings/guioptions.vim
@@ -0,0 +1,2 @@
+" Disable the macvim toolbar
+set guioptions-=T
