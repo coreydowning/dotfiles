3d4258592e3e0b1ce3ba74d80fb14ccdec0b9cb2
Corey Downing <downing.c@gmail.com>
2015-02-11 11:43:25 -0700

    make it betters

--- setup.sh
+++ setup.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash -e
 
-ln -sf $(pwd)/zshrc ${HOME}/.zshrc
-ln -sf $(pwd)/vimrc ${HOME}/.vimrc
-ln -sf $(pwd)/vim ${HOME}/.vim
-ln -sf $(pwd)/gitconfig ${HOME}/.gitconfig
-ln -sf $(pwd)/gitignore ${HOME}/.gitignore
+dir=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+echo "Installing coreydowning's dotfiles to ${HOME} from ${dir}"
+
+if [ ! -d ${dir}/vim/bundle/vundle ]
+then
+  echo "Getting Vundle..."
+  git clone -q https://github.com/gmarik/Vundle.vim.git ${dir}/vim/bundle/vundle
+fi
+
+echo "Setting up vim"
+ln -sf ${dir}/vimrc ${HOME}/.vimrc
+rm ${HOME}/.vim
+ln -sf ${dir}/vim ${HOME}/.vim
+mv ${dir}/vim/settings/appearance.vim ${dir}/vim/settings/appearance.ignore
+vim +PluginInstall +qall
+mv ${dir}/vim/settings/appearance.ignore ${dir}/vim/settings/appearance.vim
+
+echo "Setting up zsh"
+ln -sf ${dir}/zshrc ${HOME}/.zshrc
+
+echo "Setting up git"
+ln -sf ${dir}/gitconfig ${HOME}/.gitconfig
+ln -sf ${dir}/gitignore ${HOME}/.gitignore
+
+echo "DONE!"
+
---
+++ vim/settings/smart-paste.vim
@@ -0,0 +1,2 @@
+" use ,p to paste without messing up indentation
+map <Leader>p :set paste<CR>o<esc>:r !pbpaste<CR>:set nopaste<CR>
